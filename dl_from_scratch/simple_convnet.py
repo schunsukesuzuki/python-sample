1ea845df9a4103f874971a559c084d7db2e059cf
schunsukesuzuki <schunsuke.suzuki@gmail.com>
2021-04-03 10:04:06 +0900

    initial commit

--- dl_from_scratch/simple_convnet.py
+++ dl_from_scratch/simple_convnet.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append('C:\\Users\\schunsuke_2020\\Desktop\\python\\python-sample\\dl_from_scratch')
 import pickle
 import numpy as np
 from collections import OrderedDict
